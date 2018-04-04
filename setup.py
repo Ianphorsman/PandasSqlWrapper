a62d6dd064fac81d533b91410f528978526ce3d1
Ian Horsman <ianphorsman@gmail.com>
2018-04-04 19:34:32 -0400

    Packaging setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='PandasSqlWrapper',
+    description="Tool to help pandas talk to mysql databases.",
+    url="https://github.com/Ianphorsman/PandasSqlWrapper",
+    author='Ian Horsman',
+    author_email='ianphorsman@gmail.com',
+    license='MIT',
+    version='0.1.0',
+    packages=['pandas_sql_wrapper'],
+    install_requires=['numpy', 'pandas', 'pymysql', 'sqlalchemy']
+)
