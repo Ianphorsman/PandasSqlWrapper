4228e0e7e9249e4f64fef5fc5d6617e4b50008f6
Ian Horsman <ianphorsman@gmail.com>
2018-07-13 18:50:17 -0400

    new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Ian Horsman',
     author_email='ianphorsman@gmail.com',
     license='MIT',
-    version='0.1.7',
+    version='0.1.8',
     packages=['pandas_sql_wrapper'],
     install_requires=['numpy', 'pandas', 'pymysql', 'sqlalchemy', 'pg8000']
 )
